75463bb4530fa37e4cb9eac0c3e6618438d135d4
Shikha Singh <shikha.singh9115@gmail.com>
2018-06-20 16:00:31 -0400

    Blog is up and running

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+#Create A Blog models
+#title
+#pub_date
+#body
+#image
+
+#Add the blog app to the settings
+
+#Create a migration
+
+#Migrate
+
+#Add to the admin
+
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    blog_image = models.ImageField(upload_to='images/')
+    body = models.CharField(max_length=200)
+    pub_date=models.DateField()
