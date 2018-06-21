cc75c27c7f7a9797b7b12c5197100d843959a2aa
Shikha Singh <shikha.singh9115@gmail.com>
2018-06-21 19:15:44 -0400

    Finally project up and running

--- blog/models.py
+++ blog/models.py
@@ -19,3 +19,12 @@ class Blog(models.Model):
     blog_image = models.ImageField(upload_to='images/')
     body = models.TextField()
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.title
+
+    def summary(self):
+        return self.body[:100]
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime('%b %e %Y')
